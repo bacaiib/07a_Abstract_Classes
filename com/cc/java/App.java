a36f65f27bd46afa43b3f2a9ed8860e90eec47d9
bacaiib <roman-frommhold@gmx.net>
2024-01-31 12:27:03 +0100

    1. Iteration Überladene Methode

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,9 +3,33 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
+        Triangle triangle = new Triangle(10.0,10.0);     
+        
+        output("------ Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Kreis: " + circle.area());
+        output("Fläche Dreieck: " + triangle.area());
+
+        output("------  Differenzen ------");
+        output("Diff. r-t :" + areaDiff(rectangle, triangle));
+        output("Diff. r-c :" + areaDiff(rectangle, circle));
+
+
        
     }
+
+    // 1.Iteration: ggf. 1000x +überladen ...
+    private static double areaDiff(Rectangle r, Triangle t){
+        return r.area() - t.area();
+    }
+
+    private static double areaDiff(Rectangle r, Circle c){
+        return r.area() - c.area();
+    }
+
 
   
 
