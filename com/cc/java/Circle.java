df542613462fac624157ee36196c65d7d238e9b1
bacaiib <roman-frommhold@gmx.net>
2024-01-31 12:27:31 +0100

    1.Iteration: ggf. 1000x +überladen

--- com/cc/java/Circle.java
+++ com/cc/java/Circle.java
@@ -3,13 +3,22 @@ package com.cc.java;
 public class Circle extends Shape{
 
     private double radius;
+
     
+    
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+
+
 
     @Override
     public double area() {
         // TODO Auto-generated method stub
         // return radius * radius * Math.PI;
-        return Math.PI * Math.pow(radius, radius);
+        return Math.PI * Math.pow(radius, 2);
     }
 
     
