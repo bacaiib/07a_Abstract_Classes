995802ef5658fcb7e2291a85dd909eef722b44a9
bacaiib <roman-frommhold@gmx.net>
2024-01-31 12:49:29 +0100

    Das geht jetzt nicht mehr

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -12,29 +12,28 @@ public class App {
         output("Fläche Rechteck: " + rectangle.area());
         output("Fläche Kreis: " + circle.area());
         output("Fläche Dreieck: " + triangle.area());
+        
+        // Das geht jetzt nicht mehr ...
+        // Shape shape = new Shape();
 
-        output("------  Differenzen ------");
+        output("------ Differenzen ------");
         output("Diff. r-t :" + areaDiff(rectangle, triangle));
         output("Diff. r-c :" + areaDiff(rectangle, circle));
+        output("Diff. c-r: " + areaDiff(circle,rectangle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
 
-
-       
     }
 
     // 1.Iteration: ggf. 1000x +überladen ...
-    private static double areaDiff(Rectangle r, Triangle t){
-        return r.area() - t.area();
+    // private static double areaDiff(Rectangle r, Triangle t){
+    //     return r.area() - t.area();
+    // }
+
+    // 2. Iteration: Polymorphie - besser!
+    private static double areaDiff(Shape s1, Shape s2){
+        return s1.area() - s2.area();
     }
-
-    private static double areaDiff(Rectangle r, Circle c){
-        return r.area() - c.area();
-    }
-
-
-  
-
-   
-
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
